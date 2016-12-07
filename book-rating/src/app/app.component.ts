e4348c0bd765e9db9bcdc0b152902e06f13300b6
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-07 10:27:45 +0100

    Displays list of books

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works! :-)';
+  books = ['Angular 2  - Einstieg', 'Stars Wars Rouge One'];
 }
