676bb1ad99b940d339ac25817dcf63df3d233b75
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-07 14:52:07 +0100

    @ViewChild(BookComponent)

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { BookComponent } from '../book/book.component';
+import 'rxjs/add/operator/filter';
 
 import { Book } from '../shared/book';
 
@@ -7,9 +9,11 @@ import { Book } from '../shared/book';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   books: Book[];
+
+  @ViewChild(BookComponent) bc: BookComponent;
 
   get count() {
     return this.books.length;
@@ -23,6 +27,14 @@ export class DashboardComponent implements OnInit {
       new Book('222', 'AngularJS', 'Goldy but Oldie', 3)
     ];
     this.books.sort();
+  }
+
+  ngAfterViewInit() {
+    this.bc.rated
+      .filter((book: Book) => book.isbn === '111')
+      .subscribe((book) => {
+        console.log(book);
+      });
   }
 
   reorderBooks(book: Book) {
